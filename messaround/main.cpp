6b64fdf8d2d6d7da727dff54ad5a9d70169bf5c6
finderfilms56 potatoe <creepypastas4days@gmail.com>
2019-04-24 12:44:05 +0200

    commit

--- messaround/main.cpp
+++ messaround/main.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+int main() 
+{
+	string name;
+	cout << "Please enter you name: ";
+	getline(cin, name);
+	cout << "\nHello " << name << " Nice to meet you" << endl;
+}
