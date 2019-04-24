a85a2c59b3000c849626f2af836dfd042fb4966f
finderfilms56 potatoe <creepypastas4days@gmail.com>
2019-04-24 15:06:05 +0200

    test

--- messaround/main.cpp
+++ messaround/main.cpp
@@ -1,3 +1,16 @@
+/*This is the begining of a text based game. Hopefully I will be able to add some Rpg elements like stats and character 
+classes.
+Here is a list of items I would like to implement eventualy, this list may change:
+
+1. character stats
+
+2. inventory
+
+3. random enemies with random stats
+
+4. sudo open world 
+
+*/
 #include <iostream>
 #include <string>
 
